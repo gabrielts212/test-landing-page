ddf2f48f9394c89864eda2578cb442f393ded4a0
gabrielts212 <gabrielts2122@gmail.com>
2024-06-15 00:55:29 -0300

    .

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
-import Header from '../components/header/header';
-import Footer from '../components/footer/footer';
+import Header from "../components/header/header";
+import Footer from "../components/footer/footer";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Footer />
-    
     </div>
   );
 }
--- src/pages/loginpage.js
+++ src/pages/loginpage.js
@@ -1,6 +1,5 @@
-import Header from '../components/header/header';
-import Login from '../components/login/login';
-
+import Header from "../components/header/header";
+import Login from "../components/login/login";
 
 export default function Home() {
   return (
